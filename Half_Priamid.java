1f8abb4dda1c3c2d85fb8f1fbe1af4fe1991ab2c
Suraj Kumar <suraj31kumar1999@gmail.com>
2022-01-20 20:50:07 +0530

    changed in code

--- Half_Priamid.java
+++ Half_Priamid.java
@@ -1,13 +1,1 @@
-package pattern;
-
-public class Half_Priamid {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i=n;i>=1;i--){         //sidha pattern print karne ke liye(i=1;i<=n;i++)
-            for (int j=1;j<=i;j++){
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
-    }
-}
+System.out.println()[D"")[D[DHello its done);
